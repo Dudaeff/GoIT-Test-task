7f7601e41594f8192ebf98bbdd6ccd92b92eeb79
Roma <r.dutkevich@gmail.com>
2023-04-25 02:06:38 +0300

    change homepage

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/TweetZone">
+        <BrowserRouter basename="/TweetZone-GoIT-test-task">
           <App />
         </BrowserRouter>
       </PersistGate>
